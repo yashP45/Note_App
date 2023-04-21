3f686c216a654a69e982d4430f8deace5575cb72
yashP45 <76097972+yashP45@users.noreply.github.com>
2023-04-21 10:00:56 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -9,10 +9,10 @@ app.use((req , res , next) => {
     res.set("Access-Control-Allow-Origin" , "*");
     res.set("Access-Control-Allow-Headers" , "*");
     res.set("Access-Control-Allow-Methods" , "*");
-    // if(res.method =="OPTIONS"){
-    //     res.status(200).end();
-    //     return;
-    // }
+    if(res.method =="OPTIONS"){
+        res.status(200).end();
+        return;
+    }
     next();
 })
 app.use(express.json());
